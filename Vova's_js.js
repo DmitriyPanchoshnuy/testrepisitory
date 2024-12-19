55aeee3601e9fbda6d742001fbb9b5bb8ef40072
FiBlu <vovateka@gmail.com>
2024-12-19 19:04:39 +0200

    add order

--- Vova's_js.js
+++ Vova's_js.js
@@ -4,11 +4,11 @@ var settings = {
     "url": "https://supermarket-ab29.restdb.io/rest/products",
     "method": "GET",
     "headers": {
-      "content-type": "application/json",
-      "x-apikey": "675b1a9ae1db92568d971b0c",
-      "cache-control": "no-cache"
+        "content-type": "application/json",
+        "x-apikey": "675b1a9ae1db92568d971b0c",
+        "cache-control": "no-cache"
     }
-  }
+}
 
 var basketlist = [];
 var products;
@@ -18,17 +18,17 @@ function addProducts(data) {
 
 $.ajax(settings).done((result) => {
     addProducts(result);
-        $.each(result, (index, element) => {
-            $(".container").append(
-                `<div class="divchik">
+    $.each(result, (index, element) => {
+        $(".container").append(
+            `<div class="divchik">
                 <h2>${element.name}</h2>
                 <img src="${element.photo_url}">
                 <p>${element.description}</p>
                 <h3>${element.price} грн</h3>
                 <button onClick="addProductToBasket('${element._id}')">BUY</button>
                 </div>`
-            )
-        }) 
+        )
+    })
 })
 
 function addProductToBasket(id) {
@@ -55,3 +55,29 @@ function reDrawBasket() {
      <h2>До сплати: ${totalprice}</h2>
     `)
 }
+
+$("#orderform").submit((e) => {
+    e.preventDefault()
+    let order = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://supermarket-ab29.restdb.io/rest/orders",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": "675b1a9ae1db92568d971b0c",
+            "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify({
+            "name": e.target['name'].value,
+            "address": e.target['address'].value,
+            "post_number": e.target['post_number'].value,
+            "status": "New",
+            "products": JSON.stringify(basketlist),
+        })
+    }
+
+    basketlist = [];
+    reDrawBasket();
+})
