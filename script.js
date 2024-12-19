661fb6c6ae0b5e41ef0c78e134ab3fe6a736fcd3
DmitriyPanchoshnuy <dmitriypanchoshnuy@gmail.com>
2024-12-19 17:33:56 +0200

    123

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const url = "https://my-json-server.typicode.com/DmitriyPanchoshnuy/testrepisitory"
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://marketplace-0cab.restdb.io/rest/products",
+  "method": "GET",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "675b1a8be1db920a18971b08",
+    "cache-control": "no-cache"
+  }
+}
 
-$.ajax(url + "/products", {
+var basketlist = []
 
-  dataType: 'json',
+var products;
+function addProducts(data) {
+  products = data;
+}
 
-  success: (result) => {
-    console.log(result)
+$.ajax(settings).done((result) => {  
+    addProducts(result)
+    console.log(products)
     $.each(result, (index, element) => {
-
       $(".container").append(`
         <div class="product">
           <img src="${element.photo_url}">
@@ -17,18 +30,36 @@ $.ajax(url + "/products", {
           <div class="price">
             Price: ${element.price}
           </div>
-          <button class='buy'>
+          <button class='buy' onclick="addProductToBasket('${element._id}')">
             Buy
           </button>
         </div>
         `
       )
-
     })
+  })
 
-  },
+function addProductToBasket(id) {
+  basketlist.push(
+    products.find((product) => {
+      return product._id == id;
+    })
+  )
 
-  error: (error) => {
-    console.log(error.statusText);
-  }
-})
+  reDrawBasket()
+}
+
+function reDrawBasket() {
+  $('.list').html('')
+  let totalprice = 0;
+  $.each(basketlist, (index, element) => {
+    $(".list").append(`
+      <p> ${element.name} | ${element.price} </p>
+    `)
+    totalprice += element.price;
+  })
+
+  $('.list').append(`
+    <h2>До сплати: ${totalprice}</h2>
+  `)
+}
